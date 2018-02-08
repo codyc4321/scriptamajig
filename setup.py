2231c36b22449a683ef918b5ae730fef06a0fc72
Childers, Cody <codchild@visa.com>
2018-02-08 10:42:41 -0600

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup 
+
+setup(
+    name='scriptamajig',
+    version='0.1.0',
+    description='A CLI for organizing and quickly locating/describing your bash aliases and whatnot',
+    url='https://github.com/codyc4321/scriptamajig_nodejs',
+    author='Cody Childers',
+    author_email='cchilderswork@gmail.com',
+    license='MIT',
+    packages=['scriptamajig'],
+    install_requires=[],
+    zip_safe=False,
+)
